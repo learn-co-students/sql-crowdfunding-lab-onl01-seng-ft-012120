4d2c9d1367df92b7606ae5b13f09d7c8634e0c15
reddevilcero <reddevil_cero@hotmail.com>
2020-02-13 20:07:49 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.name, SUM(pledges.amount) FROM users
   JOIN pledges
   ON users.id = pledges.user_id
-  GROUP BY name
+  GROUP BY users.name
   ORDER BY SUM(pledges.amount) ASC
   "
 end
